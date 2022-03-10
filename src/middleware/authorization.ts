844aa1e50e108eb6c1e6055656356eb50c738918
Marcos Venicius <marcosdevelopment.business@gmail.com>
2022-03-09 21:59:55 -0300

    create authorization middleware

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -1,20 +1,40 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JsonWebTokenError } from "jsonwebtoken";
 
 import { configs } from "../configs";
+import { PermissionHandler } from "../permissions";
 
-import { PermissionError } from "../permissions/types";
+import {
+  PermissionError,
+  PermissionsList,
+  UsersType,
+} from "../permissions/types";
 
-export const authorization = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const token = req.headers.authorization;
-  if (!token) throw new PermissionError("Missing user token");
+export const authorization =
+  (permissions: PermissionsList[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) throw new PermissionError("Missing user token");
 
-  const result = jwt.verify(token, configs.JWT_SECRET);
+      const jwtToken = token.split(" ")[1];
+      const result: { user_type: UsersType } = jwt.verify(
+        jwtToken,
+        configs.JWT_SECRET,
+      ) as any;
 
-  console.log(result);
-  next();
-};
+      PermissionHandler.verifyPermissionIsValid(result.user_type, permissions);
+
+      next();
+    } catch (err: unknown) {
+      if (err instanceof PermissionError || err instanceof JsonWebTokenError) {
+        return res.status(401).json({
+          message: err.message,
+        });
+      } else {
+        return res.status(403).json({
+          message: "Unauthorized",
+        });
+      }
+    }
+  };
