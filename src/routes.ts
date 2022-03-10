11eee60aadf7ea819da2ee81468ddd1f80d15c0f
Marcos Venicius <marcosdevelopment.business@gmail.com>
2022-03-09 22:15:28 -0300

    add routes as example

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,54 @@
 import { Router } from "express";
+import jwt from "jsonwebtoken";
+
+import { configs } from "./configs";
+import { authorization } from "./middleware/authorization";
+import { permissions } from "./permissions";
 
 const router = Router();
 
-router.post('/login', (req, res) => {
+router.post("/login/common", (_, res) => {
+  return res.status(200).json({
+    token: jwt.sign(
+      JSON.stringify({
+        user_type: "common",
+      }),
+      configs.JWT_SECRET,
+    ),
+  });
+});
 
-})
+router.post("/login/admin", (_, res) => {
+  return res.status(200).json({
+    token: jwt.sign(
+      JSON.stringify({
+        user_type: "admin",
+      }),
+      configs.JWT_SECRET,
+    ),
+  });
+});
 
-export default Router;
+router.get("/only_admins", authorization(permissions.admin), (_, res) => {
+  return res.json({
+    message: `only admins can: ${permissions.admin.join(", ")}`,
+  });
+});
+
+router.get("/only_commons", authorization(permissions.common), (_, res) => {
+  return res.json({
+    message: `only commons can: ${permissions.common.join(", ")}`,
+  });
+});
+
+router.get(
+  "/only_specific",
+  authorization(["read_user_profile", "create_user"]),
+  (_, res) => {
+    return res.json({
+      message: `only specific can: read_user_profile, create_user`,
+    });
+  },
+);
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
+import "dotenv/config";
 import express from "express";
-import { config } from "dotenv";
-import { configs } from "./configs";
 
-config();
+import { configs } from "./configs";
+import routes from "./routes";
 
 const app = express();
+
+app.use(routes);
 
 app.listen(configs.PORT, () => {
   console.log(`Running at port ${configs.PORT}`);
